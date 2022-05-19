6baeb2cd54ca75537ee327cb069dfb8816f6e42a
kkarrkid7 <kkarrkid7@gmail.com>
2022-05-19 22:16:46 +0900

    [Renew] MVP Architecture

--- index.js
+++ index.js
@@ -1,80 +1,81 @@
+"use strict"
+
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const fileupload = require('express-fileupload')
 
-
-// ----------- Router ---------------
-const itemRouter = require('./routes/items');
-const resRouter = require('./routes/res');
-
-app.listen(8000, (res)=>console.log("run")) // port 8000
-
 const reqPre = (req, res, next) => {
-	console.log(Date.now());
-	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-	console.log(fullUrl);
-	next();
+    console.log(Date.now());
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl);
+    next();
 }
-// -------------- USE --------------
-app.use('/items', itemRouter);
-app.use('/res', resRouter);
 
-app.use(fileupload({	// File Upload Setting
-	limits: { fileSize: 50 * 1024 * 1024 },
-}));
+app.use(reqPre); // 접속 Log 추적
 
-// ------------------ MODEL ---------------
-const Item = require('./models/item')	// Item Model
-const Store = require('./models/store')	// Store Model
+app.set("views", "src/views");
+app.set("view engine", "pug");
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// DB Connect 
-mongoose.connect('mongodb://localhost:27017/items', {useNewUrlParser: true, useUnifiedTopology: true})
-	.then(() => {
-		console.log("OPEN!!")
-	})
-	.catch(err => {
-		console.log("ERROR!!")
-	})
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
-app.use(reqPre); // response logs
+module.exports = app
 
-// ------------ RES -------------
 
-app.get('', async(req, res)=>{
-	const items = await Item.find({})
-	res.render('index', {items})
-})
+// // -------------- USE --------------
+// app.use(fileupload({    // File Upload Setting
+//     limits: { fileSize: 50 * 1024 * 1024 },
+// }));
 
-app.get('/test', (req, res) => {
-	res.render('./res/ex');	// android main test case
-});
 
-app.get('/test2', async (req, res) => {
-	const stores = await Store.find({})
-	res.json(stores);
-});
+// // DB Connect 
+// mongoose.connect('mongodb://localhost:27017/items', {useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => {
+//         console.log("OPEN!!")
+//     })
+//     .catch(err => {
+//         console.log("ERROR!!")
+//     })
 
-app.get('/image', (req, res) => {
-	res.render('./upload');	// file upload test page
-	console.log('get')
-});
+// app.use(reqPre); // response logs
 
-app.post('/image', (req, res) => {	// file upload server
+// // ------------ RES -------------
 
-	if (!req.files || Object.keys(req.files).length === 0) {
-		return res.status(400).send('no files upload');
-	}
+// app.get('', async(req, res)=>{
+//     const items = await Item.find({})
+//     res.render('index', {items})
+// })
 
-	uploadFile = req.files.image
-	uploadPath = __dirname + '/views/res/' + uploadFile.name;
+// app.get('/test', (req, res) => {
+//     res.render('./res/ex'); // android main test case
+// });
 
-	uploadFile.mv(uploadPath, (err) => {
-		res.send('Upload');
-	});
-});
+// app.get('/test2', async (req, res) => {
+//     const stores = await Store.find({})
+//     res.json(stores);
+// });
+
+// app.get('/image', (req, res) => {
+//     res.render('./upload'); // file upload test page
+//     console.log('get')
+// });
+
+// app.post('/image', (req, res) => {  // file upload server
+
+//     if (!req.files || Object.keys(req.files).length === 0) {
+//         return res.status(400).send('no files upload');
+//     }
+
+//     uploadFile = req.files.image
+//     uploadPath = __dirname + '/views/res/' + uploadFile.name;
+
+//     uploadFile.mv(uploadPath, (err) => {
+//         res.send('Upload');
+//     });
+// });
--- routes/items.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-router.get('/', (req, res, next) => {
-	res.send({item: "item"});// res.render('res/index', {});
-})
-
-module.exports = router
