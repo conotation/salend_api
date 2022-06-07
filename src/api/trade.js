ae9a05a3725031ce482dc2e919021877763fe04f
kkarrkid7 <kkarrkid7@gmail.com>
2022-06-07 21:44:03 +0900

    [db] buylist add

--- src/api/trade.js
+++ src/api/trade.js
@@ -8,12 +8,12 @@ const Buy = require('../models/buy')
 
 tradeRouter.get('/debug/:id', (req, res) => {
     let response = {};
-    Item.findOne({_id: req.params.id})
-    .then(item => {
-        console.log(item)
-        let value = {item: item.i_name, value: item.i_now_price, id: req.params.id}
-        res.render('pay', value)
-    })
+    Item.findOne({ _id: req.params.id })
+        .then(item => {
+            console.log(item)
+            let value = { item: item.i_name, value: item.i_now_price, id: req.params.id }
+            res.render('pay', value)
+        })
 }); // 결제 페이지
 
 tradeRouter.get('/cancel/:id', (req, res) => {
@@ -22,32 +22,52 @@ tradeRouter.get('/cancel/:id', (req, res) => {
     const imp_success = req.query.imp_success
     const error_msg = req.query.error_msg
 
-    Item.findOne({_id: req.params.id})
-    .then(item => {
-        if(item == null)
-            res.json({success: false, msg:"Id에 해당하는 상품이 없습니다."})
+    const updateItem = { i_status: 4 }
+    Item.findByIdAndUpdate(req.params.id, updateItem)
+        .then(item => {
+            Item.findOne({ _id: req.params.id })
+                .then(item => {
+                    if (item == null)
+                        res.json({ success: false, msg: "Id에 해당하는 상품이 없습니다." })
 
-        let data = {
-            name: item.i_name,
-            price: item.i_now_price, 
-            imp: imp_uid, 
-            merchant: merchant_uid, 
-            success: imp_success,
-            msg: "" + error_msg
-        }
+                    if (imp_success) {
+                        const newBuy = new Store {
+                            b_ref: imp_uid,
+                            b_item: item,
+                        }
 
-        res.render('cancel', data)
-    })
-})
+                        newBuy.save()
+                            .then(p => {
+                                console.log("==== new Buy ====")
+                                console.log(p)
+                            })
+                            .catch(e => {
+                                console.log(e)
+                            })
+                    }
+
+                    let data = {
+                        name: item.i_name,
+                        price: item.i_now_price,
+                        imp: imp_uid,
+                        merchant: merchant_uid,
+                        success: imp_success,
+                        msg: "" + error_msg
+                    }
+
+                    res.render('cancel', data)
+                })
+        })
+});
 
 tradeRouter.post('/:id', (req, res) => {
     let response = {};
-    Item.findOne({_id: req.params.id})
-    .then(item => {
-        console.log(item)
-        let value = {item: item.i_name, value: item.i_now_price}
-        res.render('pay', value)
-    })
+    Item.findOne({ _id: req.params.id })
+        .then(item => {
+            console.log(item)
+            let value = { item: item.i_name, value: item.i_now_price }
+            res.render('pay', value)
+        })
 }); // 결제 페이지
 
 
